eddddc2ba83f953659a2b74e4418deb0f8ff668f
Your Name <you@example.com>
2019-01-13 19:26:39 +0100

    update6

--- install-debian2.sh
+++ install-debian2.sh
@@ -15,7 +15,9 @@ sudo mount -t sysfs /sys /mnt/debian/sys
 sudo mount -o bind /dev /mnt/debian/dev
 cat << EOF | sudo chroot /mnt/debian
 apt-get update
-echo -e "a\nyes\n" | apt-get install --no-install-recommends --force-yes --yes linux-image-586 systemd-sysv grub2-common grub-pc && echo "LABEL=DEBUSB / ext4 defaults 0 1" > /etc/fstab
+apt-get install --no-install-recommends --force-yes --yes linux-image-586 systemd-sysv
+echo -e "1\n" | apt-get install --no-install-recommends --force-yes --yes grub2-common grub-pc
+echo "LABEL=DEBUSB / ext4 defaults 0 1" > /etc/fstab
 grub-install --target=i386-pc --boot-directory=/boot --force-file-id --skip-fs-probe --recheck ${LO_DEVICE}
 #Your Configuration
 apt-get install --no-install-recommends --force-yes --yes sudo nano vim git perl openssh-server openssh-client ncftp network-manager
