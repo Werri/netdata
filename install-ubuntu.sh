9faaf98ed4732ca20b63acabdfe1294dbf8a7cc2
Your Name <you@example.com>
2019-01-24 19:23:38 +0100

    Changes

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -23,11 +23,11 @@ apt-get install --no-install-recommends --force-yes --yes libblkid1
 apt-get install --no-install-recommends --force-yes --yes usbmount
 apt-get install --no-install-recommends --force-yes --yes initramfs-tools
 dd bs=512 count=1 if=/dev/sda of=./mbr_backup.img
-echo -e "1\n" | apt-get install --no-install-recommends --force-yes --yes grub2-common grub-pc
-echo "LABEL=DEBUSB / ext4 defaults 0 1" > /etc/fstab
+echo -e '1\n' | apt-get install --no-install-recommends --force-yes --yes grub2-common grub-pc
+echo 'LABEL=DEBUSB / ext4 defaults 0 1' > /etc/fstab
 grub-install --target=i386-pc –-root-directory=/ --boot-directory=/boot --force-file-id --skip-fs-probe --recheck ${LO_DEVICE}
 #Your Configuration
-echo -e "\n\n\n\n\n\n\n\n\n\n\n\n\n\n" | apt-get install --no-install-recommends --force-yes --yes locales
+echo -e '\n\n\n\n\n\n\n\n\n\n\n\n\n\n' | apt-get install --no-install-recommends --force-yes --yes locales
 locale-gen --purge de_DE.UTF-8
 echo -e 'LC_ALL="de_DE.UTF-8"\nLANG="de_DE.UTF-8"\nLANGUAGE="de_DE:de"\n' > /etc/default/locale
 LC_ALL="de_DE.UTF-8"
@@ -45,9 +45,9 @@ echo -e '\n\n\n2\n2\n2\n2\n2\n2\n2\n2\n2\n2\n2\n2\n' | apt-get install --no-inst
 localectl set-keymap --no-convert de
 loadkeys de
 apt-get install --no-install-recommends --force-yes --yes  nano vim git perl openssh-server openssh-client ncftp network-manager
-echo -e "root\nroot\n" | passwd root
+echo -e 'root\nroot\n' | passwd root
 adduser --quiet --system --group --disabled-password --shell /bin/bash --home /home/debian --gecos "Full name,Room number,Work phone,Home phone" debian
-echo -e "debian\ndebian\n" | passwd debian
+echo -e 'debian\ndebian\n' | passwd debian
 adduser debian
 echo -e '\n\n\nset default="0"\nset timeout=10\nmenuentry "Debian" {\n    linux /vmlinuz root=/dev/disk/by-label/DEBUSB quiet\n    initrd /initrd.img\n}\n\n\n' >> /etc/grub.d/40_custom
 echo 'GRUB_DISABLE_LINUX_UUID=true' >> /etc/default/grub
@@ -55,14 +55,14 @@ echo 'GRUB_ENABLE_LINUX_LABEL=true' >> /etc/default/grub
 update-grub
 apt-get install --no-install-recommends --force-yes --yes parted
 e2label ${LO_DEVICE} DEBUSB
-echo -e "#!/bin/bash\nmount -t proc proc proc/\nmount -t sysfs sys sys/\nmount -o bind /dev dev/" > /chrootme.sh
-echo -e "#!/bin/bash\nexit\numount ./{dev,sys,proc}\numount .\n" > /unchrootme.sh
+echo -e '#!/bin/bash\nmount -t proc proc proc/\nmount -t sysfs sys sys/\nmount -o bind /dev dev/' > /chrootme.sh
+echo -e '#!/bin/bash\nexit\numount ./{dev,sys,proc}\numount .\n' > /unchrootme.sh
 chmod 755 /chrootme.sh
 chown root:root /chrootme.sh
 chmod 755 /unchrootme.sh
 chown root:root /unchrootme.sh
-echo "LABEL=DEBUSB / ext4 rw,suid,dev,exec,auto,nouser,async,errors=continue 0 1" > /etc/fstab
-# echo "proc /proc proc rw,suid,dev,exec,auto,nouser,async,errors=continue 0 0" >> /etc/fstab
+echo 'LABEL=DEBUSB / ext4 rw,suid,dev,exec,auto,nouser,async,errors=continue 0 1' > /etc/fstab
+# echo 'proc /proc proc rw,suid,dev,exec,auto,nouser,async,errors=continue 0 0' >> /etc/fstab
 dd bs=512 count=1 if=./mbr_backup.img of=/dev/sda
 echo ${CF_FILE1} |  awk '{system($0)}'
 exit
